f827b4de3be7f4859a6d0ce29e413142131ffc32
cromazurek <camillemazurek23@gmail.com>
2025-08-07 12:56:26 -0500

    updated script

--- vcf_annotater.sh
+++ vcf_annotater.sh
@@ -7,8 +7,8 @@ VCF=$1
 
 REF="/gpfs1/projects/Tisza_Lab/crm_flu_mutatome/H1N1_align/reference/H1N1_reference.fasta"
 GFF="/gpfs1/projects/Tisza_Lab/crm_flu_mutatome/H1N1_align/reference/gff/GCA_039113225.1_ASM3911322v1_genomic.gff"
+SAMPLE = $(basename $VCF .vcf.gz)
 
-
-bcftools csq -f $REF -g $GFF $VCF -Ob -o annotated.bcf
-bcftools view annotated.bcf -Oz -o annotated.vcf.gz
-bcftools index annotated.vcf.gz
+bcftools csq -f $REF -g $GFF $VCF -Ob -o ${SAMPLE}.annotated.bcf
+bcftools view ${SAMPLE}.annotated.bcf -Oz -o ${SAMPLE}.annotated.vcf.gz
+bcftools index ${SAMPLE}.annotated.vcf.gz

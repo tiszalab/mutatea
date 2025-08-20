cf1f3a09909e5de21689a7d74f4cd049f6270669
cromazurek <camillemazurek23@gmail.com>
2025-08-20 12:24:28 -0500

    adapting old_h1n1_align.sh script

--- old_h1n1_align.sh
+++ old_h1n1_align.sh
@@ -6,17 +6,17 @@
 ## use bcftools to get vcf files
 
 POOLID=$1
+OUTPUT_DIR="/gpfs1/projects/Tisza_Lab/crm_flu_mutatome/H1N1_align/${POOLID}"
 
 
 if [ ! -d ${POOLID} ] ; then
-	mkdir ${POOLID}
+	mkdir -p ${OUTPUT_DIR}
 fi
 
-source /cmmr/prod/envParams/condanewenv.init && conda activate crm_pangenome
+source /cmmr/prod/envParams/condanewenv.init && conda activate crm_flutatome
 
-REF="/gpfs1/projects/Tisza_Lab/crm_flu_mutatome/H1N1_align/reference/H1N1_reference.fasta"
+REF="/gpfs1/projects/Tisza_Lab/crm_flu_mutatome/H1N1_align/reference/H1N1_reference_clean.fasta"
 R1_LIST=$( find /gpfs1/projects/Pools/EsViritu/${POOLID} -type f -name "*H1N1.R1.fastq" )
-
 
 if [ ! -z "$R1_LIST" ] ; then
 
@@ -31,20 +31,23 @@ if [ ! -z "$R1_LIST" ] ; then
 
 		SAMPLE=$( echo $R1_BASE | cut -d "." -f 1 )
 
-		## minimap2
-        /gpfs1/projects/Tisza_Lab/crm_flu_mutatome/H1N1_align/minimap2-2.30_x64-linux/minimap2 -ax sr $REF $READ1 $READ2 > ${TMPDIR}/${SAMPLE}.sam 
+		# minimap2
+        minimap2 -ax sr $REF $READ1 $READ2 > ${OUTPUT_DIR}/${SAMPLE}.sam 
 
-        ## samtools sort and index
-        samtools view -@ 48 -bS  ${TMPDIR}/${SAMPLE}.sam > ${TMPDIR}/${SAMPLE}.${POOLID}.bam
-		samtools sort -@ 48 -o ${POOLID}/${SAMPLE}.${POOLID}.sort.bam ${TMPDIR}/${SAMPLE}.${POOLID}.bam
-        samtools index ${POOLID}/${SAMPLE}.${POOLID}.sort.bam
+        # samtools sort and index
+        samtools view -@ 48 -bS  ${OUTPUT_DIR}/${SAMPLE}.sam > ${OUTPUT_DIR}/${SAMPLE}.${POOLID}.bam
+		samtools sort -@ 48 -o ${OUTPUT_DIR}/${SAMPLE}.${POOLID}.sort.bam ${OUTPUT_DIR}/${SAMPLE}.${POOLID}.bam
+        samtools index ${OUTPUT_DIR}/${SAMPLE}.${POOLID}.sort.bam
 
-        ## bcftools to get vcf
-        BAM="${POOLID}/${SAMPLE}.${POOLID}.sort.bam"
+        # Set bam variable
+        BAM="${OUTPUT_DIR}/${SAMPLE}.${POOLID}.sort.bam"
+
         # Create a VCF file for the sample
-        bcftools mpileup -Ou -f $REF $BAM | bcftools call -mv -Oz -o ${POOLID}/${SAMPLE}.vcf.gz
+        bcftools mpileup -Ou -f $REF -q 0 -Q 0 --min-BQ 0 --max-depth 1000000 -a DP,AD $BAM | bcftools view -Oz -o ${OUTPUT_DIR}/${SAMPLE}.${POOLID}.vcf.gz
         
-        # Index the VCF file
-        bcftools index ${POOLID}/${SAMPLE}.vcf.gz
+        #bcftools norm -Oz -o ${OUTPUT_DIR}/${SAMPLE}.${POOLID}.vcf
+        
+        # crm set to DNR Index the VCF file
+        # bcftools index ${OUTPUT_DIR}/${SAMPLE}.${POOLID}.vcf.gz
 done
 fi

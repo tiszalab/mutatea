871448a136b965a4ed519d6263cc764c85e621ae
cromazurek <camillemazurek23@gmail.com>
2025-12-10 17:17:54 -0600

    test

--- python_CLI/metadata_processing.py
+++ python_CLI/metadata_processing.py
@@ -2,6 +2,8 @@
 # Confirm the user has all required modules installed
 print("Please check the module_requirements.txt file and install any missing modules with pip install -r module_requirements.txt")
 
+# test commit to see if custom hotkeys for windsurf have been added
+
 # load modules
 import pandas as pd
 import glob
